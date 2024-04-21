13c43dda236f8365dac42f80359b5553c0056d41
diksha-jeena <158588723+diksha-jeena@users.noreply.github.com>
2024-04-21 20:14:00 +0530

    inserting values

--- animal.sql
+++ animal.sql
@@ -1,4 +1,13 @@
+#creating database
 CREATE DATABASE ANIMALS ;
+#using database
 USE ANIMALS ; 
+#creating table
 CREATE TABLE CAT
 (ID INT NOT NULL , NAME VARCHAR(25) , BREED VARCHAR(25) , COLORATION VARCHAR(25) , AGE INT , SEX VARCHAR(20) , FAV_TOY VARCHAR(25));
+INSERT INTO CAT
+VALUES
+(1 , "Charlie" , "Abyssinian" , "cream" , 2 , "male" , "Ball" ),
+(2 , "Lily" , "American Curl" , "white" , 1 , "female" , "puzzle" ),
+(3 , "Kitty" , "American Curl" , "white" , 2 , "female" , "feather toy"),
+(4 , "Shadow" , "Bombay" , "black" , 1 , "male" , "chew toys" );
