006fb03388ca03a166598a328eb2e31e74e44aa7
diksha-jeena <158588723+diksha-jeena@users.noreply.github.com>
2024-04-24 19:10:01 +0530

    using logical operators

--- animal.sql
+++ animal.sql
@@ -1,14 +1,18 @@
-#CREATING DATABASE 
+#creating database
 CREATE DATABASE ANIMALS ;
-#USING DATABASE 
+#using database
 USE ANIMALS ; 
-#CREATING TABLE
+#creating table
 CREATE TABLE CAT
 (ID INT NOT NULL , NAME VARCHAR(25) , BREED VARCHAR(25) , COLORATION VARCHAR(25) , AGE INT , SEX VARCHAR(20) , FAV_TOY VARCHAR(25));
-#INSERTING VALUES
 INSERT INTO CAT
 VALUES
 (1 , "Charlie" , "Abyssinian" , "cream" , 2 , "male" , "Ball" ),
 (2 , "Lily" , "American Curl" , "white" , 1 , "female" , "puzzle" ),
 (3 , "Kitty" , "American Curl" , "white" , 2 , "female" , "feather toy"),
 (4 , "Shadow" , "Bombay" , "black" , 1 , "male" , "chew toys" );
+
+SELECT ID , NAME
+FROM CAT 
+WHERE(5 > AGE OR AGE > 10)
+AND BREED = 'American Curl' ;
